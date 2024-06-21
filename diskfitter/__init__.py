d55be17f4799941c451585e765bddf709940c36b
jinshisai <jn.insa.sai@gmail.com>
2024-06-21 21:27:43 +0800

    Remove export

--- diskfitter/__init__.py
+++ diskfitter/__init__.py
@@ -4,6 +4,6 @@ from .models import ThreeLayerDisk, SingleLayerDisk
 from . import models
 from . import mpe
 from . import grid
-from . import export
+#from . import export
 
-__all__ = ['Fitter', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid', 'export']
+__all__ = ['Fitter', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid',]
