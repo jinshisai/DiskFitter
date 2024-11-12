107a4220c8642b55c39e9f58446f05f9b3bdc85f
jinshisai <jn.insa.sai@gmail.com>
2024-11-12 17:42:49 +0800

    Put all Cpython functions to libcube

--- dimo/libcube/setup.py
+++ dimo/libcube/setup.py
@@ -5,7 +5,7 @@ import numpy
 
 setup(
     name = 'ttldisk',
-    ext_modules = cythonize('ttldisk.pyx'),
+    ext_modules = cythonize('linecube.pyx'),
     include_dirs = [numpy.get_include()],
     define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],
     )
