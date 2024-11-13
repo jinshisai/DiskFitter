eebb521f1dbdf441995ba4c386f906e7b9c4b475
jinshisai <jn.insa.sai@gmail.com>
2024-11-13 16:50:45 +0800

    Added libcube

--- dimo/__init__.py
+++ dimo/__init__.py
@@ -4,6 +4,7 @@ from .models import ThreeLayerDisk, SingleLayerDisk, MultiLayerDisk
 from . import models
 from . import mpe
 from . import grid
+from . import libcube
 #from . import export
 
-__all__ = ['DiMO', 'MultiLayerDisk', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid',]
+__all__ = ['DiMO', 'MultiLayerDisk', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid', 'libcube']
