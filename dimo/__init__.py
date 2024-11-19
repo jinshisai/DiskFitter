2bea10de7b1a92bced07b95338f5a5ec75e9c5f7
jinshisai <jn.insa.sai@gmail.com>
2024-11-20 00:14:15 +0800

    Added FitThinModel

--- dimo/__init__.py
+++ dimo/__init__.py
@@ -1,5 +1,5 @@
 # import model
-from ._dimo import DiMO
+from ._dimo import DiMO, FitThinModel
 from .models import ThreeLayerDisk, SingleLayerDisk, MultiLayerDisk
 from . import models
 from . import mpe
@@ -7,4 +7,4 @@ from . import grid
 from . import libcube
 #from . import export
 
-__all__ = ['DiMO', 'MultiLayerDisk', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid', 'libcube']
+__all__ = ['DiMO', 'FitThinModel', 'MultiLayerDisk', 'ThreeLayerDisk', 'SingleLayerDisk', 'models', 'mpe', 'grid', 'libcube']
